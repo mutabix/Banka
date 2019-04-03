8b478c4b1872656099c001a8dc384b344f9be886
Moise1 <moiseri01@gmail.com>
2019-04-04 01:41:12 +0200

    admin can remove a user's account

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -3,4 +3,16 @@ const dashboard = document.getElementById('dashboard-info');
 
 const accounts = document.getElementById('accounts').addEventListener('click', () => {
     dashboard.innerHTML = '<object height="1000" width="1129" type="text/html" data="accounts.html"></object>';
-});
+})
+
+
+function deleteRow(button) {
+    const option = confirm('Are you sure to delete this account?');
+    
+    if (option === true) {
+        const tableRow = button.parentNode.parentNode;
+        tableRow.parentNode.removeChild(tableRow);
+    }
+    return false;
+
+}
